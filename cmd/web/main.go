1c4de77fc34a79ea5eff58924a97574d4cf9f105
alperergul <alper.ergul.97@gmail.com>
2023-03-21 23:01:11 +0300

    final websocket

---
+++ cmd/web/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"websockets/internal/handlers"
+)
+
+func main() {
+
+	mux := routes()
+
+
+	log.Println("Starting channel listener")
+	go handlers.ListenToWsChannel()
+
+	log.Println("Starting web server on port 8080")
+	_ = http.ListenAndServe(":8080", mux)
+
+}
---
+++ cmd/web/routes.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net/http"
+	"websockets/internal/handlers"
+
+	"github.com/bmizerany/pat"
+)
+
+func routes() http.Handler {
+
+	mux := pat.New()
+
+	mux.Get("/", http.HandlerFunc(handlers.Home))
+	mux.Get("/ws", http.HandlerFunc(handlers.WsEndpoint))
+
+
+	fileServer := http.FileServer(http.Dir("./static/"))
+	mux.Get("/static/",http.StripPrefix("/static",fileServer))
+
+	return mux
+}
